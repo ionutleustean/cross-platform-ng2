475437a7bcca74cfdf5475a66ae862654fa0d63b
Ionut <ionut.leustean@outlook.com>
2016-08-17 01:20:31 +0300

    router + bootstrap + header + internet status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
 import {ViewEncapsulation} from '@angular/core';
+import {ROUTER_DIRECTIVES, provideRouter,  RouterConfig} from '@angular/router';
+
+
+import {OnlinePage} from './pages/online/Online';
+
+
+
+
+export const routes: RouterConfig = [
+  {
+    path: '',
+    component: OnlinePage
+  }
+];
+
+export const APP_ROUTER_PROVIDERS = [
+  provideRouter(routes)
+];
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  directives: [ROUTER_DIRECTIVES]
 })
+
 export class AppComponent {
   title = 'app works!';
+  internetStatus = false;
+
+  constructor(){
+  }
+
+  getInternetStatus(){
+    this.internetStatus = navigator.onLine ;
+    let self = this;
+    window.addEventListener("offline", function(e) { self.internetStatus =  navigator.onLine });
+    window.addEventListener("online", function(e) { self.internetStatus =  navigator.onLine });
+
+    if(this.internetStatus === true){
+      return "label-success";
+    }else{
+      return "label-danger";
+    }
+  }
 }
+
+
+//
+// export const routes: RouterConfig = [
+//   {
+//     path: '',
+//     component: OnlinePage
+//   }
+// ];
+//
+
+
+
+
---
+++ src/app/pages/online/Online.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'online-page',
+    templateUrl: 'Online.html',
+    // styleUrls: ['app.component.css'],
+})
+export class OnlinePage {
+    title = 'OnlinePage!';
+}
+
+
+
+
+
+
